7fdb9e776080b606a0e1ad2a2b85e9825160da05
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-04-02 13:52:13 +0100

    implemented header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <main className="bg-neutral900 min-h-screen font-poppins px-4 pt-4 pb-10 flex flex-col md:px-8 md:pt-5 md:pb-16 gap-6 lg:gap-8 lg:py-6 lg:px-25">
+    <main className="bg-neutral100 min-h-screen font-poppins px-4 pt-4 pb-10 flex flex-col md:px-8 md:pt-5 md:pb-16 gap-6 lg:gap-8 lg:py-6 lg:px-25">
       <Header/>
     </main>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,16 @@
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="flex items-center gap-3 justify-between">
+        <div>
+            <img src="./logo-small.svg" alt="" className="w-10 h-10 md:hidden"/>
+            <img src="./logo-large.svg" alt="" className="hidden md:inline-block"/>
+        </div>
+        <div className="flex gap-1 items-center p-1 border border-neutral900 bg-neutral0 shadow-header rounded-full">
+            <button className="px-3 py-2 bg-yellow500 border border-neutral900 text-preset4 font-semibold text-neutral900 rounded-full">Study Mode</button>
+            <button className="px-3 py-2 text-preset4 font-semibold text-neutral900 rounded-full">All Cards</button>
+        </div>
+    </header>
   )
 }
 
